c2882a202da07933f70e2773a9240f5a941ee009
Mathius Johnson <devachievem@gmail.com>
2020-08-07 21:31:47 +0000

    stretch not completed but the rest is

--- vampire.js
+++ vampire.js
@@ -10,22 +10,39 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let numberOfVampires = 0;
+    let currentVampire = this;
 
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      numberOfVampires++;
+    }
+    return numberOfVampires;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    let currentVampire = this;
+    console.log(currentVampire.creator);
+    if (this.creator === null) {
+      return true;
+    }
+    if (currentVampire === vampire.creator) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   /** Stretch **/
@@ -38,6 +55,7 @@ class Vampire {
   closestCommonAncestor(vampire) {
 
   }
+
 }
 
 module.exports = Vampire;
